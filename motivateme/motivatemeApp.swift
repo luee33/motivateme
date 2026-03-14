9d2e3621bc1eb74e8e95bf39e338fb1a7d173ffa
Luis <luee33@gmail.com>
2026-03-14 16:56:41 -0700

    Initial Commit

---
+++ motivateme/motivatemeApp.swift
@@ -0,0 +1,17 @@
+//
+//  motivatemeApp.swift
+//  motivateme
+//
+//  Created by Luis Yuja on 3/14/26.
+//
+
+import SwiftUI
+
+@main
+struct motivatemeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
